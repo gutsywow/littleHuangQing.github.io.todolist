d59a39f9fa0604beecd4a9a66f1d34d46d2a627d
24481 <qing.huang01@hand-china.com>
2019-07-24 17:45:41 +0800

    Updates

---
+++ precache-manifest.9ea6a26a069f978fb0d1b843dd56bf32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a72cc989989db500a656",
+    "url": "/static/js/main.a72cc989.chunk.js"
+  },
+  {
+    "revision": "ee29916f341f365ee5dd",
+    "url": "/static/js/1.ee29916f.chunk.js"
+  },
+  {
+    "revision": "a72cc989989db500a656",
+    "url": "/static/css/main.ac7bbd88.chunk.css"
+  },
+  {
+    "revision": "cf10e0888ce58ae8b57026629670a98a",
+    "url": "/index.html"
+  }
+];
